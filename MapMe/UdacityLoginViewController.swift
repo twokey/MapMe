9337ac8537ce093ca5d2ed76f2f91aa26a9f747e
twokey <twokey@node-1w7jr9qpbvd66y8ctqrldlhak.ipv6.telus.net>
2017-04-06 22:17:29 -0700

    Feat: Setup Alert VC; Implement errors check for login VC

--- MapMe/UdacityLoginViewController.swift
+++ MapMe/UdacityLoginViewController.swift
@@ -17,6 +17,7 @@ class UdacityLoginViewController: UIViewController {
     @IBOutlet weak var userEmail: UITextField!
     @IBOutlet weak var userPassword: UITextField!
     @IBOutlet weak var fbLoginButton: FBSDKLoginButton!
+    @IBOutlet weak var udacityLoginButton: UIButton!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     // MARK: Lifecycle
@@ -40,43 +41,48 @@ class UdacityLoginViewController: UIViewController {
         }
     }
     
-    func loginWithFBToken(_ fbToken: String) {
-        
-        // Set up UI
-        activityIndicator.isHidden = false
-        activityIndicator.startAnimating()
-
-        // Get user ID using FB token
-        UdacityClient.sharedInstance().postSessionWithFacebookToken(fbToken) { userID, error in
-                
-            if let userID = userID {
-                guard let userID = Int(userID) else {
-                    return
-                }
-                
-                self.loginWithUserID(userID)
-            }
-        }
-    }
-    
     func loginWithUserID(_ userID: Int) {
         
         
         // Login with user ID
         UdacityClient.sharedInstance().getUdacityUserPublicData(userID) { user, error in
             
-            // TODO: Add guards for errors
+            guard (error == nil) else {
+                print(error ?? "Error was not provided")
+                performUIUpdatesOnMain {
+                    self.activityIndicator.stopAnimating()
+                    self.interfaceDimmed(false)
+                    AllertViewController.showAlertWithTitle("User Profile", message: "Cannot get user profile information. Try again")
+                }
+                return
+            }
+            
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
             
             // Get reference to App Delegate where we keep our model
             if let user = user {
-                let appDelegate = UIApplication.shared.delegate as! AppDelegate
                 appDelegate.user = user
             }
             
-            // Get student's locations and links
+            // Get student's locations and links; We will need them for next VC and a user is waiting anyway
             UdacityClient.sharedInstance().getStudents() { students, error in
 
+                guard (error == nil) else {
+                    print(error ?? "Error was not provided")
+                    performUIUpdatesOnMain {
+                        self.activityIndicator.stopAnimating()
+                        self.interfaceDimmed(false)
+                        AllertViewController.showAlertWithTitle("Students Data", message: "Cannot download students locations")
+                    }
+                    return
+                }
+                
                 guard let students = students else {
+                    performUIUpdatesOnMain {
+                        self.activityIndicator.stopAnimating()
+                        self.interfaceDimmed(false)
+                        AllertViewController.showAlertWithTitle("Students Data", message: "Cannot download students locations")
+                    }
                     return
                 }
 
@@ -100,49 +106,127 @@ class UdacityLoginViewController: UIViewController {
                     annotation.subtitle = mediaURL
 
                     // Save student information (annotations) in app delegate
-                    let appDelegate = UIApplication.shared.delegate as! AppDelegate
                     appDelegate.annotations.append(annotation)
                 }
                 
                 // We have user data, students data (annotations) we can continue to map VC
                 performUIUpdatesOnMain {
                     self.activityIndicator.stopAnimating()
+                    self.interfaceDimmed(false)
                     self.performSegue(withIdentifier: "userAuthorized", sender: self)
                 }
             }
         }
     }
     
-    
-    // MARK: Actions
-    
-    @IBAction func udacityLogin(_ sender: UIButton) {
+    func loginWithFBToken(_ fbToken: String) {
         
-        // Set up UI
- //       activityIndicator.isHidden = false
+        // Setup UI
         activityIndicator.startAnimating()
-
+        interfaceDimmed(true)
         
-        guard let userName = userEmail.text else {
-            print("userEmail text field is nil")
-            return
-        }
-        
-        guard let userPassword = userPassword.text else {
-            print("userPassword is nil")
-            return
-        }
-        
-        // Login with Udacity credentials
-        UdacityClient.sharedInstance().getUdacityStudentIDforUser(userName, userPassword: userPassword) { userID, error in
+        // Get user ID using FB token
+        UdacityClient.sharedInstance().postSessionWithFacebookToken(fbToken) { userID, error in
+            
+            guard (error == nil) else {
+                print(error ?? "Error was not provided")
+                performUIUpdatesOnMain {
+                    self.activityIndicator.stopAnimating()
+                    self.interfaceDimmed(false)
+                    AllertViewController.showAlertWithTitle("Facebook Login", message: "Cannot login with Facebook")
+                }
+                return
+            }
             
             if let userID = userID {
                 guard let userID = Int(userID) else {
+                    performUIUpdatesOnMain {
+                        self.activityIndicator.stopAnimating()
+                        self.interfaceDimmed(false)
+                        AllertViewController.showAlertWithTitle("User ID", message: "User ID is not Int")
+                    }
                     return
                 }
                 
                 self.loginWithUserID(userID)
+            }
+        }
+    }
+    
+    
+    // MARK: UI setup helpers
+    
+    func interfaceDimmed(_ state: Bool) {
+        
+        if state {
+            // Disable and dim interface
+            let alpha = CGFloat(0.5)
+            userEmail.isEnabled = !state
+            userEmail.alpha = alpha
+            userPassword.isEnabled = !state
+            userPassword.alpha = alpha
+            fbLoginButton.isEnabled = !state
+            fbLoginButton.alpha = alpha
+            udacityLoginButton.isEnabled = !state
+            udacityLoginButton.alpha = alpha
+        } else {
+            // Enable interface
+            let alpha = CGFloat(1)
+            userEmail.isEnabled = !state
+            userEmail.alpha = alpha
+            userPassword.isEnabled = !state
+            userPassword.alpha = alpha
+            fbLoginButton.isEnabled = !state
+            fbLoginButton.alpha = alpha
+            udacityLoginButton.isEnabled = !state
+            udacityLoginButton.alpha = alpha
+        }
+    }
+    
+    
+    // MARK: Actions
+    
+    // Login with Udacity credentials
+    @IBAction func udacityLogin(_ sender: UIButton) {
+        
+        guard let userName = userEmail.text, userName != "" else {
+            AllertViewController.showAlertWithTitle("Account", message: "Please enter Udacity email")
+            return
+        }
+        
+        guard let userPassword = userPassword.text, userPassword != "" else {
+            AllertViewController.showAlertWithTitle("Password", message: "Please enter password for Udacity account")
+            return
+        }
+
+        // Setup UI
+        activityIndicator.startAnimating()
+        interfaceDimmed(true)
+
+        // Login with Udacity credentials
+        UdacityClient.sharedInstance().getUdacityStudentIDforUser(userName, userPassword: userPassword) { userID, error in
+            
+            guard (error == nil) else {
+                print(error ?? "Error was not provided")
+                performUIUpdatesOnMain {
+                    self.activityIndicator.stopAnimating()
+                    self.interfaceDimmed(false)
+                    AllertViewController.showAlertWithTitle("User ID", message: "Cannot get user ID. Try again")
+                }
+                return
+            }
+            
+            if let userID = userID {
+                guard let userID = Int(userID) else {
+                    performUIUpdatesOnMain {
+                        self.activityIndicator.stopAnimating()
+                        self.interfaceDimmed(false)
+                        AllertViewController.showAlertWithTitle("User ID", message: "User ID is not Int")
+                    }
+                    return
+                }
                 
+                self.loginWithUserID(userID)
             }
         }
     }
@@ -166,9 +250,9 @@ extension UdacityLoginViewController: FBSDKLoginButtonDelegate {
         let graphRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil)
         graphRequest?.start() { connection, result, error in
             if error != nil {
-                print(error!)
+                print(error ?? "Error was not provided")
             } else {
-               print(result!)
+               print(result ?? "No result provided")
             }
         }
     }
